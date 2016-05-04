16bb8ba041a084d4cd7c1ea27d5b39807e3db8fb
onissachar <asachar@cca.edu>
2016-05-04 12:27:29 -0700

    commit

--- jQuery_Example/js/myscript.js
+++ jQuery_Example/js/myscript.js
@@ -2,19 +2,15 @@ $(function() {
 
        $topIndex = 0;
 
-       $( ".ui-draggable" ).draggable().css({"position":"absolute","top":"0px" });;
+       $( ".ui-draggable" ).draggable().css({"position":"absolute","top":"-10px", left:"0" });;
+
 
         $(".ui-draggable").click(function(){
             $topIndex=$topIndex+1;
             $(this).css("z-index",$topIndex);
         });
 
+     
+  
+
    });
-
-
-        $(function(){
-          $.okvideo({ source: 'https://www.youtube.com/watch?v=LM8vHH-9vsE',
-                    volume: 100})
-        });
-
-        
